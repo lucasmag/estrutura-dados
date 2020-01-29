df17b2846a95a5c92f786f1e5d3d0dc0c3965b3a
Alunos <Alunos>
2020-01-29 16:38:52 -0300

    Modificação primeira questão

--- Lista00/1.c
+++ Lista00/1.c
@@ -6,8 +6,16 @@ int main (){
     int b[5] = {5, 15, 25, 35, 45};
     int c[10];
 
-    memcpy(c, a, sizeof(a));
-    memcpy(c + 5, b, sizeof(b));
+    //memcpy(c, a, sizeof(a));
+    //memcpy(c + 5, b, sizeof(b));
+    
+    for(int j = 0; j < 10 ; j++){
+    	if(j < 5){
+    		c[j] = a[j];
+		} else {
+			c[j] = b[j-5];
+		}
+	}
 
     for(int i = 0 ; i < 10 ; i++){
         printf("%d ", c[i]);
