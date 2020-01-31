d2d871ec27894c446830c59b97491e4a18ecabc2
Lucas Sousa <lucasmag97@gmail.com>
2020-01-30 21:25:09 -0300

    Correções pontuais no desafio 1

--- Lista01/1.c
+++ Lista01/1.c
@@ -1,21 +1,20 @@
 // Solution of URI 1176 - Fibonacci Array
-
 #include <stdio.h>
 
 int main () {
     int T, N, i;
     unsigned long long int fib [60];
 
-    fib [0] = 0;
-    fib [1] = 1;
+    fib[0] = 0;
+    fib[1] = 1;
 
     for(i = 2; i <= 60; i++){
-        fib [i] = fib [i-1] + fib [i-2];
+        fib[i] = fib[i-1] + fib[i-2];
     }
 
     scanf("%d", &T);
 
-    for(i = 1; i <= T; i++){
+    for(i = 0; i < T; i++){
         scanf("%d", &N);
         printf("Fib(%d) = %llu\n", N, fib[N]);
     }
