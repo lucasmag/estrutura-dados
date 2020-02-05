57f60b6a483329534afe83d8cc3395184ff736b6
Alunos <Alunos>
2020-02-05 17:22:55 -0300

    Reestruturação 4 questao

--- Lista01/4.c
+++ Lista01/4.c
@@ -11,23 +11,16 @@ int main(){
             for(j = 1 ; j <= N ; j++){
                 x = i;
 
-                if(j < x){
-					x = j;
-				}
-				if(N-i+1 < x) {
-					x = N-i+1;
-				}
-				if(N-j+1 < x){
-					x = N-j+1;
-				}
+                if(j < x){ x = j; }
+                
+				if(N - i + 1 < x) { x = N - i + 1; }
+				
+				if(N - j + 1 < x){ x = N - j + 1; }
 
                 printf("%3d", x);
 
-                if(j < N){
-					printf(" ");
-				} else{
-					printf("\n");
-				}
+                if(j < N){ printf(" "); } 
+				else{ printf("\n"); }
             }
         }
         printf("\n");
